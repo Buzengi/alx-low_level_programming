608b593cfc2d62e38996191a5c207f7f89ebfed6
Chidi <ajaerohc@gmail.com>
2022-09-08 17:42:44 -0700

    Q4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *
+ * Description: Prints out a given string to stdout
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
