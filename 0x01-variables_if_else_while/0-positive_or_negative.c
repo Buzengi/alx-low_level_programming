4258b41f00d0270c1c3c3ea63c42a045e219cb51
Chidi <ajaerohc@gmail.com>
2022-09-10 04:46:06 -0700

    Q0 recommit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,10 @@
 /**
  * main - program will assign a random number to the variable
  *
+ * if the number is greater than 0: is positive
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative
+ *
  *Return: Always 0
  */
 
@@ -16,19 +20,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("%d\n", n);
 
-	if (n < 0)
-	{
-		printf("is negative");
-	}
-
-	if (n == 0)
-	{
-		printf("is Zero");
-	}
-
 	if (n > 0)
 	{
-		printf("is positive")
+		printf("The number is positive");
+	}
+
+	else if (n == 0)
+	{
+		printf("The number is Zero");
+	}
+	
+	else
+	{
+		printf("The number is negative");
 	}
 
 	return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase and UPPERCASE
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	while (lowercase <= 'z')
+	{
+		putchar (lowercase);
+		lowercase++;
+	}
+
+	putchar ('\n');
+
+	char uppercase = 'A';
+	while (uppercase <= 'Z')
+	{
+		putchar (uppercase);
+		uppercase++;
+	}
+	putchar ('\n');
+
+	Return(0);
+}
