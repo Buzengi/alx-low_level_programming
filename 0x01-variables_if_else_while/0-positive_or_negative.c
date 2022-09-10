bb2cdb0f8d4907983cad74111aefbf422b9f23d4
Chidi <ajaerohc@gmail.com>
2022-09-09 18:35:03 -0700

    Q0 commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - program will assign a random number to the variable
+ *
+ *Return: Always 0
+*/ 
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d \n", x);
+	return (0);
+}
