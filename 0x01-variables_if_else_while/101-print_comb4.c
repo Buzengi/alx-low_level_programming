393168d144195044d0f602e4a32b3acb70637eb9
Chidi <ajaerohc@gmail.com>
2022-09-10 21:59:20 -0700

    Q6 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * Description: prints all possible different combinations of three digits
+ * return: Always 0 (success)
+ */
+
+
