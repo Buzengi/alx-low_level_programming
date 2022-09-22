17a27ebe2178a92a1a0a06a838f8e23593b75c60
Chidi <ajaerohc@gmail.com>
2022-09-21 18:46:24 -0700

    strcat commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int dest_len = 0, src_len = 0, index = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; index <= src_len; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
