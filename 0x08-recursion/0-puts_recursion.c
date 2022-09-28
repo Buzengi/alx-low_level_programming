39f332ce97e490ec4d465eee9eb1561313505476
Chidi <ajaerohc@gmail.com>
2022-09-28 14:33:55 -0700

    recursion task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts_recursion - func that prints a string
- * @*s: pointer to the string
+ * @s: pointer to the string
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s[0] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(*s[0]);
 	_puts_recursion(s + 1);
-	_putchar(s[0]);
 }
