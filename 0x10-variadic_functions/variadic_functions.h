af1cd788f42a87823c29f2ea049d6be719a150e5
Chidi <ajaerohc@gmail.com>
2022-10-13 21:31:38 -0700

    Write a function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct pt - Struct of printer type
+ * @c: Type to print
+ * @pf: The function associated
+ * Return:
+ */
+
+typedef struct pt
+{
+	char c;
+	void (*pf)(va_list);
+} f_print;
+
+void (*get_printer(char f))(va_list);
+void p_strings(va_list arg);
+void p_float(va_list arg);
+void p_int(va_list arg);
+void p_char(va_list arg);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
